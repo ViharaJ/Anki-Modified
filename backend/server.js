61d77c9f1e996abb8580e5ffcb6871a1918d369b
vihara <vihara@students.cs.ubc.ca>
2023-01-06 21:05:56 -0800

    initalized everything

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+// listen
+app.listen(4000, () => {
+    console.log('listening!')
+})
--- script.js
+++
@@ -1,3 +0,0 @@
-function createCard(){
-    document.getElementsByClassName("newCard")
-}
